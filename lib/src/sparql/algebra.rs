8b539ab7ac49ecaa141ddb80e5929f15f54c33c7
Tpt <thomas@pellissier-tanon.fr>
2019-11-21 13:41:08 +0100

    CI: Always update rustup

--- lib/src/sparql/algebra.rs
+++ lib/src/sparql/algebra.rs
@@ -770,10 +770,9 @@ impl<'a> fmt::Display for SparqlGraphPattern<'a> {
                 SparqlGraphPattern(&*a),
                 SparqlGraphPattern(&*b),
             ),
-            #[rustfmt::skip] // TODO: Discrepancy between the latest cargo fmt and the one on GitHub CI
             GraphPattern::Graph(g, p) => {
                 write!(f, "GRAPH {} {{ {} }}", g, SparqlGraphPattern(&*p),)
-            },
+            }
             GraphPattern::Extend(p, v, e) => write!(
                 f,
                 "{} BIND({} AS {})",
