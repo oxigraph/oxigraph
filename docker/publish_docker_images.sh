94f2bc49296ede3660b84521c02a34bbcba4cf5c
maxlath <k@maxlath.eu>
2020-07-17 23:49:50 +0200

    docker: move Dockerfile to server and wikibase folder
    and change builder image to rust:1-buster
    as recommended in https://github.com/oxigraph/oxigraph/pull/41#discussion_r456384441

--- docker/publish_docker_images.sh
+++ docker/publish_docker_images.sh
@@ -4,8 +4,8 @@ if [[ -z ./Dockerfile ]] ; then
   exit 1
 fi
 
-cat Dockerfile | docker build -t oxigraph/oxigraph -
-cat Dockerfile | sed s/oxigraph_server/oxigraph_wikibase/g | docker build -t oxigraph/oxigraph-wikibase -
+cat server/Dockerfile | docker build -t oxigraph/oxigraph -
+cat wikibase/Dockerfile | docker build -t oxigraph/oxigraph-wikibase -
 
 docker push oxigraph/oxigraph:latest
 docker push oxigraph/oxigraph-wikibase:latest
