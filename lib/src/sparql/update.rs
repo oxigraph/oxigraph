7bb41e655e0ef1ad3f9527008eb805b5e8de0f75
Tpt <thomaspt@hotmail.fr>
2021-11-27 20:15:01 +0100

    SPARQL: we do not need to ensure strings exist anymore

--- lib/src/sparql/update.rs
+++ lib/src/sparql/update.rs
@@ -144,12 +144,6 @@ impl SimpleUpdateEvaluator {
                     Self::convert_ground_quad_pattern(quad, &variables, &tuple, &dataset)?
                 {
                     self.transaction.remove(quad.as_ref())?;
-                    if !insert.is_empty() {
-                        // Hack to make sure the triple terms are still available for an insert
-                        dataset.encode_term(quad.subject.as_ref());
-                        dataset.encode_term(quad.predicate.as_ref());
-                        dataset.encode_term(quad.object.as_ref());
-                    }
                 }
             }
             for quad in insert {
