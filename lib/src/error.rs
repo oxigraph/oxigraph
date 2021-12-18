db4c4bcb97642f0c5fb5e00fc6794220ae5a8410
Tpt <thomaspt@hotmail.fr>
2021-12-18 12:01:06 +0100

    Removes dead code

--- lib/src/error.rs
+++ lib/src/error.rs
@@ -1,24 +1,5 @@
-use std::convert::Infallible;
 use std::error::Error;
 use std::io;
-
-/// Traits that allows unwrapping only infallible results
-pub trait UnwrapInfallible {
-    type Value;
-
-    fn unwrap_infallible(self) -> Self::Value;
-}
-
-impl<T> UnwrapInfallible for Result<T, Infallible> {
-    type Value = T;
-
-    fn unwrap_infallible(self) -> T {
-        match self {
-            Ok(value) => value,
-            Err(error) => match error {},
-        }
-    }
-}
 
 pub fn invalid_data_error(error: impl Into<Box<dyn Error + Send + Sync>>) -> io::Error {
     io::Error::new(io::ErrorKind::InvalidData, error)
