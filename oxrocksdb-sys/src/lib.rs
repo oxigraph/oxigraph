f04b8a0fb453b9e11394251ef0d54f6deea4b953
Tpt <thomaspt@hotmail.fr>
2022-03-15 19:37:40 +0100

    Hides bindgen generated code warnings

--- oxrocksdb-sys/src/lib.rs
+++ oxrocksdb-sys/src/lib.rs
@@ -1,3 +1,8 @@
-#![allow(non_upper_case_globals, non_camel_case_types, non_snake_case)]
+#![allow(
+    non_upper_case_globals,
+    non_camel_case_types,
+    non_snake_case,
+    deref_nullptr
+)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
