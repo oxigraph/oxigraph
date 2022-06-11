ed5004a70d18a6ad9aa5f1d05d224dff865d423a
Tpt <thomaspt@hotmail.fr>
2022-06-11 13:49:33 +0200

    Fixes RocksDB build

--- oxrocksdb-sys/api/build_version.cc
+++ oxrocksdb-sys/api/build_version.cc
@@ -3,34 +3,36 @@
 #include <memory>
 
 #include "rocksdb/version.h"
-#include "util/string_util.h"
+#include "rocksdb/utilities/object_registry.h"
+
 
 namespace ROCKSDB_NAMESPACE {
-const std::unordered_map<std::string, std::string>& GetRocksBuildProperties() {
-  static std::unique_ptr<std::unordered_map<std::string, std::string>> props(new std::unordered_map<std::string, std::string>());
-  return *props;
-}
+    std::unordered_map<std::string, RegistrarFunc> ObjectRegistry::builtins_ = {};
 
-std::string GetRocksVersionAsString(bool with_patch) {
-  std::string version = ToString(ROCKSDB_MAJOR) + "." + ToString(ROCKSDB_MINOR);
-  if (with_patch) {
-    return version + "." + ToString(ROCKSDB_PATCH);
-  } else {
-    return version;
-  }
-}
-  
-std::string GetRocksBuildInfoAsString(const std::string& program, bool verbose) {
-  std::string info = program + " (RocksDB) " + GetRocksVersionAsString(true);
-  if (verbose) {
-    for (const auto& it : GetRocksBuildProperties()) {
-      info.append("\n    ");
-      info.append(it.first);
-      info.append(": ");
-      info.append(it.second);
+    const std::unordered_map<std::string, std::string>& GetRocksBuildProperties() {
+        static std::unique_ptr<std::unordered_map<std::string, std::string>> props(new std::unordered_map<std::string, std::string>());
+        return *props;
     }
-  }
-  return info;
-}
-} // namespace ROCKSDB_NAMESPACE
 
+    std::string GetRocksVersionAsString(bool with_patch) {
+        std::string version = std::to_string(ROCKSDB_MAJOR) + "." + std::to_string(ROCKSDB_MINOR);
+        if (with_patch) {
+            return version + "." + std::to_string(ROCKSDB_PATCH);
+        } else {
+            return version;
+        }
+    }
+
+    std::string GetRocksBuildInfoAsString(const std::string& program, bool verbose) {
+        std::string info = program + " (RocksDB) " + GetRocksVersionAsString(true);
+        if (verbose) {
+            for (const auto& it : GetRocksBuildProperties()) {
+                info.append("\n    ");
+                info.append(it.first);
+                info.append(": ");
+                info.append(it.second);
+            }
+        }
+        return info;
+    }
+} // namespace ROCKSDB_NAMESPACE
