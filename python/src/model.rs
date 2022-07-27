cec55b22de7e14b2387303229ede192bc66d463e
Edmond <edmond.chuc@outlook.com>
2022-07-27 13:40:58 +0000

    Revert "Add pyoxigraph type annotations for model module"
    This reverts commit 41d79d734c45716199c591df6b3e641106920bcd.

--- python/src/model.rs
+++ python/src/model.rs
@@ -19,7 +19,7 @@ use std::vec::IntoIter;
 ///
 /// >>> str(NamedNode('http://example.com'))
 /// '<http://example.com>'
-#[pyclass(name = "NamedNode", module = "oxigraph", subclass)]
+#[pyclass(name = "NamedNode", module = "oxigraph")]
 #[pyo3(text_signature = "(value)")]
 #[derive(Eq, PartialEq, Ord, PartialOrd, Debug, Clone, Hash)]
 pub struct PyNamedNode {
@@ -121,7 +121,7 @@ impl PyNamedNode {
 ///
 /// >>> str(BlankNode('ex'))
 /// '_:ex'
-#[pyclass(name = "BlankNode", module = "oxigraph", subclass)]
+#[pyclass(name = "BlankNode", module = "oxigraph")]
 #[pyo3(text_signature = "(value)")]
 #[derive(Eq, PartialEq, Debug, Clone, Hash)]
 pub struct PyBlankNode {
@@ -234,7 +234,7 @@ impl PyBlankNode {
 /// '"example"@en'
 /// >>> str(Literal('11', datatype=NamedNode('http://www.w3.org/2001/XMLSchema#integer')))
 /// '"11"^^<http://www.w3.org/2001/XMLSchema#integer>'
-#[pyclass(name = "Literal", module = "oxigraph", subclass)]
+#[pyclass(name = "Literal", module = "oxigraph")]
 #[pyo3(text_signature = "(value, *, datatype = None, language = None)")]
 #[derive(Eq, PartialEq, Debug, Clone, Hash)]
 pub struct PyLiteral {
@@ -353,7 +353,7 @@ impl PyLiteral {
 }
 
 /// The RDF `default graph name <https://www.w3.org/TR/rdf11-concepts/#dfn-default-graph>`_.
-#[pyclass(name = "DefaultGraph", module = "oxigraph", subclass)]
+#[pyclass(name = "DefaultGraph", module = "oxigraph")]
 #[derive(Eq, PartialEq, Debug, Clone, Copy, Hash)]
 pub struct PyDefaultGraph {}
 
@@ -531,7 +531,7 @@ impl IntoPy<PyObject> for PyTerm {
 /// A triple could also be easily destructed into its components:
 ///
 /// >>> (s, p, o) = Triple(NamedNode('http://example.com'), NamedNode('http://example.com/p'), Literal('1'))
-#[pyclass(name = "Triple", module = "oxigraph", subclass)]
+#[pyclass(name = "Triple", module = "oxigraph")]
 #[derive(Eq, PartialEq, Debug, Clone, Hash)]
 #[pyo3(text_signature = "(subject, predicate, object)")]
 pub struct PyTriple {
@@ -708,7 +708,7 @@ impl IntoPy<PyObject> for PyGraphName {
 /// A quad could also be easily destructed into its components:
 ///
 /// >>> (s, p, o, g) = Quad(NamedNode('http://example.com'), NamedNode('http://example.com/p'), Literal('1'), NamedNode('http://example.com/g'))
-#[pyclass(name = "Quad", module = "oxigraph", subclass)]
+#[pyclass(name = "Quad", module = "oxigraph")]
 #[pyo3(text_signature = "(subject, predicate, object, graph_name = None)")]
 #[derive(Eq, PartialEq, Debug, Clone, Hash)]
 pub struct PyQuad {
@@ -870,7 +870,7 @@ impl PyQuad {
 ///
 /// >>> str(Variable('foo'))
 /// '?foo'
-#[pyclass(name = "Variable", module = "oxigraph", subclass)]
+#[pyclass(name = "Variable", module = "oxigraph")]
 #[pyo3(text_signature = "(value)")]
 #[derive(Eq, PartialEq, Debug, Clone, Hash)]
 pub struct PyVariable {
