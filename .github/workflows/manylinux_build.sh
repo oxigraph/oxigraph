82495e2a9832f86ea56469ae64eef2ebf73ae561
Tpt <thomaspt@hotmail.fr>
2022-12-21 20:33:25 +0100

    Manylinux  wheels: install rustup "by hand"

--- .github/workflows/manylinux_build.sh
+++ .github/workflows/manylinux_build.sh
@@ -2,8 +2,11 @@ cd /workdir
 yum -y install centos-release-scl-rh
 yum -y install llvm-toolset-7.0
 source scl_source enable llvm-toolset-7.0
-curl https://sh.rustup.rs -sSf | sh -s -- -y --profile minimal
-export PATH="${PATH}:/root/.cargo/bin:/opt/python/cp37-cp37m/bin:/opt/python/cp38-cp38/bin:/opt/python/cp39-cp39/bin:/opt/python/cp310-cp310/bin:/opt/python/cp311-cp311/bin"
+curl https://static.rust-lang.org/rustup/dist/%arch%-unknown-linux-gnu/rustup-init --output rustup-init
+chmod +x rustup-init
+./rustup-init -y --profile minimal
+source "$HOME/.cargo/env"
+export PATH="${PATH}:/opt/python/cp37-cp37m/bin:/opt/python/cp38-cp38/bin:/opt/python/cp39-cp39/bin:/opt/python/cp310-cp310/bin:/opt/python/cp311-cp311/bin"
 cd python
 python3.10 -m venv venv
 source venv/bin/activate
