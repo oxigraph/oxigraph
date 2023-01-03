f376f99f6fad2799840c9018b2d00df26621260a
Tpt <thomaspt@hotmail.fr>
2023-01-03 20:54:32 +0100

    SPARQL test: displays query results diffs
    Makes debugging easier

--- testsuite/src/report.rs
+++ testsuite/src/report.rs
@@ -13,40 +13,41 @@ pub struct TestResult {
 }
 
 pub fn dataset_diff(expected: &Dataset, actual: &Dataset) -> String {
-    let (_, changeset) = diff(
+    format_diff(
         &normalize_dataset_text(expected),
         &normalize_dataset_text(actual),
-        "\n",
-    );
-    let mut ret = String::new();
-    ret.push_str("Note: missing quads in yellow and extra quads in blue\n");
-    for seq in changeset {
-        match seq {
-            Difference::Same(x) => {
-                ret.push_str(&x);
-                ret.push('\n');
-            }
-            Difference::Add(x) => {
-                ret.push_str("\x1B[94m");
-                ret.push_str(&x);
-                ret.push_str("\x1B[0m");
-                ret.push('\n');
-            }
-            Difference::Rem(x) => {
-                ret.push_str("\x1B[93m");
-                ret.push_str(&x);
-                ret.push_str("\x1B[0m");
-                ret.push('\n');
-            }
-        }
-    }
-    ret
+        "quads",
+    )
 }
 
 fn normalize_dataset_text(store: &Dataset) -> String {
     let mut quads: Vec<_> = store.iter().map(|q| q.to_string()).collect();
     quads.sort();
     quads.join("\n")
+}
+
+pub(super) fn format_diff(expected: &str, actual: &str, kind: &str) -> String {
+    let (_, changeset) = diff(expected, actual, "\n");
+    let mut ret = String::new();
+    writeln!(
+        &mut ret,
+        "Note: missing {kind} in yellow and extra {kind} in blue"
+    )
+    .unwrap();
+    for seq in changeset {
+        match seq {
+            Difference::Same(x) => {
+                writeln!(&mut ret, "{x}").unwrap();
+            }
+            Difference::Add(x) => {
+                writeln!(&mut ret, "\x1B[94m{x}\x1B[0m").unwrap();
+            }
+            Difference::Rem(x) => {
+                writeln!(&mut ret, "\x1B[93m{x}\x1B[0m").unwrap();
+            }
+        }
+    }
+    ret
 }
 
 #[allow(unused_must_use)]
