f15101a2b34856dcb92a8329bd9d54963281ea0d
Tpt <thomaspt@hotmail.fr>
2023-02-16 09:39:36 +0100

    Js: Uses Rome instead of JS standard
    Provides a built-in linter.
    
    Some formatting parameter are overriden to align with Rust and Python.

--- js/build_package.js
+++ js/build_package.js
@@ -1,31 +1,29 @@
 #! /usr/bin/env node
 
-const fs = require('fs')
+const fs = require("fs");
 
 // We copy file to the new directory
-fs.mkdirSync('pkg')
-for (const file of fs.readdirSync('./pkg-web')) {
-  fs.copyFileSync('./pkg-web/' + file, './pkg/' + file)
+fs.mkdirSync("pkg");
+for (const file of fs.readdirSync("./pkg-web")) {
+    fs.copyFileSync(`./pkg-web/${file}`, `./pkg/${file}`);
 }
-for (const file of fs.readdirSync('./pkg-node')) {
-  fs.copyFileSync('./pkg-node/' + file, './pkg/' + file)
+for (const file of fs.readdirSync("./pkg-node")) {
+    fs.copyFileSync(`./pkg-node/${file}`, `./pkg/${file}`);
 }
 
-const pkg = JSON.parse(fs.readFileSync('./pkg/package.json'))
-pkg.name = 'oxigraph'
-pkg.main = 'node.js'
-pkg.browser = 'web.js'
-pkg.files = [
-  '*.{js,wasm,d.ts}'
-]
-pkg.homepage = 'https://github.com/oxigraph/oxigraph/tree/main/js'
+const pkg = JSON.parse(fs.readFileSync("./pkg/package.json"));
+pkg.name = "oxigraph";
+pkg.main = "node.js";
+pkg.browser = "web.js";
+pkg.files = ["*.{js,wasm,d.ts}"];
+pkg.homepage = "https://github.com/oxigraph/oxigraph/tree/main/js";
 pkg.bugs = {
-  url: 'https://github.com/oxigraph/oxigraph/issues'
-}
-pkg.collaborators = undefined
+    url: "https://github.com/oxigraph/oxigraph/issues",
+};
+pkg.collaborators = undefined;
 pkg.repository = {
-  type: 'git',
-  url: 'https://github.com/oxigraph/oxigraph.git',
-  directory: 'js'
-}
-fs.writeFileSync('./pkg/package.json', JSON.stringify(pkg, null, 2))
+    type: "git",
+    url: "https://github.com/oxigraph/oxigraph.git",
+    directory: "js",
+};
+fs.writeFileSync("./pkg/package.json", JSON.stringify(pkg, null, 2));
