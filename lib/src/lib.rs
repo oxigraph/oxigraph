34b6e7a6b74b27323a5b49e21b3ee82fea7bdabc
Tpt <thomaspt@hotmail.fr>
2023-07-08 15:09:41 +0200

    Properly document features in docs.rs

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,8 +1,8 @@
 #![doc = include_str!("../README.md")]
+#![doc(test(attr(deny(warnings))))]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
 #![doc(html_favicon_url = "https://raw.githubusercontent.com/oxigraph/oxigraph/main/logo.svg")]
 #![doc(html_logo_url = "https://raw.githubusercontent.com/oxigraph/oxigraph/main/logo.svg")]
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
-#![doc(test(attr(deny(warnings))))]
 
 pub mod io;
 pub mod sparql;
