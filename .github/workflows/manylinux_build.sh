b1538f5fcb04e9d27878a169242b23f1f865f343
Tpt <thomaspt@hotmail.fr>
2023-08-19 11:09:56 +0200

    Improves naming of artifacts built by the CI

--- .github/workflows/manylinux_build.sh
+++ .github/workflows/manylinux_build.sh
@@ -8,14 +8,14 @@ chmod +x rustup-init
 source "$HOME/.cargo/env"
 export PATH="${PATH}:/opt/python/cp37-cp37m/bin:/opt/python/cp38-cp38/bin:/opt/python/cp39-cp39/bin:/opt/python/cp310-cp310/bin:/opt/python/cp311-cp311/bin"
 cd python
-python3.10 -m venv venv
+python3.11 -m venv venv
 source venv/bin/activate
 pip install -r requirements.dev.txt
-maturin develop --release -m Cargo.toml
+maturin develop --release
 python generate_stubs.py pyoxigraph pyoxigraph.pyi --black
-maturin build --release -m Cargo.toml --features abi3 --compatibility manylinux2014
+maturin build --release --features abi3 --compatibility manylinux2014
 if [ %for_each_version% ]; then
   for VERSION in 7 8 9 10 11; do
-    maturin build --release -m Cargo.toml --interpreter "python3.$VERSION" --compatibility manylinux2014
+    maturin build --release --interpreter "python3.$VERSION" --compatibility manylinux2014
   done
 fi
