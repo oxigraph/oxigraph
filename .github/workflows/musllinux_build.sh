9b985295ae6366b96ef7986cf1bc7df50e6ee087
Tpt <thomaspt@hotmail.fr>
2023-09-21 08:29:41 +0200

    Drops Musl linux 1.1 support
    Follows Rust move

--- .github/workflows/musllinux_build.sh
+++ .github/workflows/musllinux_build.sh
@@ -11,9 +11,9 @@ source venv/bin/activate
 pip install -r requirements.dev.txt
 maturin develop --release
 python generate_stubs.py pyoxigraph pyoxigraph.pyi --black
-maturin build --release --features abi3 --compatibility musllinux_1_1
+maturin build --release --features abi3 --compatibility musllinux_1_2
 if [ %for_each_version% ]; then
   for VERSION in 7 8 9 10 11; do
-    maturin build --release --interpreter "python3.$VERSION" --compatibility musllinux_1_1
+    maturin build --release --interpreter "python3.$VERSION" --compatibility musllinux_1_2
   done
 fi
