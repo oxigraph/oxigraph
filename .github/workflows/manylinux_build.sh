70b1c52166b36e80dc5caafd1e0db36038b290aa
Tpt <thomaspt@hotmail.fr>
2023-10-06 11:03:04 +0200

    Python: uses 3.12 in CI + builds for it

--- .github/workflows/manylinux_build.sh
+++ .github/workflows/manylinux_build.sh
@@ -8,14 +8,14 @@ chmod +x rustup-init
 source "$HOME/.cargo/env"
 export PATH="${PATH}:/opt/python/cp37-cp37m/bin:/opt/python/cp38-cp38/bin:/opt/python/cp39-cp39/bin:/opt/python/cp310-cp310/bin:/opt/python/cp311-cp311/bin"
 cd python
-python3.11 -m venv venv
+python3.12 -m venv venv
 source venv/bin/activate
 pip install -r requirements.dev.txt
 maturin develop --release
 python generate_stubs.py pyoxigraph pyoxigraph.pyi --black
 maturin build --release --no-default-features --features abi3 --features rustls --compatibility manylinux2014
 if [ %for_each_version% ]; then
-  for VERSION in 7 8 9 10 11; do
+  for VERSION in 8 9 10 11 12; do
     maturin build --release --no-default-features --features rustls --interpreter "python3.$VERSION" --compatibility manylinux2014
   done
 fi
