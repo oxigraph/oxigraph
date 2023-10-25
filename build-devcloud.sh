f1975a221521f3968694c462c7b47a3ef9254a93
D. A. Pellegrino <don@decisym.ai>
2023-10-25 12:41:00 -0400

    Issue #2 WIP - Moved compiler configuration specific to Intel Compiler from config.toml to build-devcloud.sh as environment variables so that it does not interfere with other build tools.

--- build-devcloud.sh
+++ build-devcloud.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
+# Use with `source build-devcloud.sh` to setup environment variables
+# that control compilation and linking on the Intel(R) DevCloud with the
+# Intel(R) oneAPI DPC++/C++ Compiler.
+
+# Use the Intel(R) oneAPI DPC++/C++ Compiler
 export CC=icx
 export CXX=icpx
+
+# Use libclang
 export LIBCLANG_PATH="${ONEAPI_ROOT}"/tensorflow/latest/lib
+
+# Use clang headers
 export CPATH="${CPATH}":"${ONEAPI_ROOT}"/compiler/latest/linux/lib/clang/17/include
-mkdir "${PBS_SCRATCHDIR}"/tmp
+
+# Use local device storage for temp files instead of NFS to
+# work-around Rust API errors from NFS mounted files not deleting as
+# expected.
+mkdir --parents "${PBS_SCRATCHDIR}"/tmp
 export TMPDIR="${PBS_SCRATCHDIR}"/tmp
-export RUSTFLAGS=--cfg=rustix_use_libc
+
+# For the rustix library use libc. Use clang and lld for linking.
+export RUSTFLAGS="--cfg=rustix_use_libc -C linker=${CMPLR_ROOT}/linux/bin-llvm/clang -C link-arg=-fuse-ld=lld"
