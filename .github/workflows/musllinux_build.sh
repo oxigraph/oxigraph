97acf45d9403b78e7401314f13ca646725efdd2b
Tpt <thomaspt@hotmail.fr>
2023-11-04 20:51:54 +0100

    Uses Ruff instead of Black

--- .github/workflows/musllinux_build.sh
+++ .github/workflows/musllinux_build.sh
@@ -10,7 +10,7 @@ python3.12 -m venv venv
 source venv/bin/activate
 pip install -r requirements.dev.txt
 maturin develop --release
-python generate_stubs.py pyoxigraph pyoxigraph.pyi --black
+python generate_stubs.py pyoxigraph pyoxigraph.pyi --ruff
 maturin build --release --no-default-features --features abi3 --features rustls --compatibility musllinux_1_2
 if [ %for_each_version% ]; then
   for VERSION in 8 9 10 11 12; do
