4affa5b17885e1b2c0e0fb1d419bd98500516501
Tpt <thomaspt@hotmail.fr>
2024-01-24 21:46:21 +0100

    Release v0.4.0-alpha.3

--- .github/workflows/musllinux_build.sh
+++ .github/workflows/musllinux_build.sh
@@ -9,11 +9,11 @@ cd python
 python3.12 -m venv venv
 source venv/bin/activate
 pip install -r requirements.dev.txt
-maturin develop --release --no-default-features --features rustls
+maturin develop --release
 python generate_stubs.py pyoxigraph pyoxigraph.pyi --ruff
-maturin build --release --no-default-features --features abi3 --features rustls --compatibility musllinux_1_2
+maturin build --release --features abi3 --compatibility musllinux_1_2
 if [ %for_each_version% ]; then
   for VERSION in 8 9 10 11 12; do
-    maturin build --release --no-default-features --features rustls --interpreter "python3.$VERSION" --compatibility musllinux_1_2
+    maturin build --release --interpreter "python3.$VERSION" --compatibility musllinux_1_2
   done
 fi
