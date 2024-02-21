4d2861c6f03450dfcb7642ca2ae250bbd0d16453
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2024-02-21 11:30:06 +0000

    chore: fix error message

--- lib/oxrdf/src/cast_error.rs
+++ lib/oxrdf/src/cast_error.rs
@@ -1,11 +1,11 @@
 use std::error::Error;
 
-/// An error return if some content in the database is corrupted.
+/// An error return if trying to cast a term as something it cannot be converted to.
 #[derive(Debug, thiserror::Error)]
 #[error(transparent)]
 pub struct TermCastError(#[from] pub TermCastErrorKind);
 
-/// An error return if some content in the database is corrupted.
+/// An error return if trying to cast a term as something it cannot be converted to.
 #[derive(Debug, thiserror::Error)]
 pub enum TermCastErrorKind {
     #[error("{0}")]
