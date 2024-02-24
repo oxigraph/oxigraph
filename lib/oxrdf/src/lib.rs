dc3a277c75c2928eeb79794b353c9c06ceaa4b3d
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2024-02-24 12:45:13 +0000

    chore: run cargo fmt

--- lib/oxrdf/src/lib.rs
+++ lib/oxrdf/src/lib.rs
@@ -17,7 +17,7 @@ mod variable;
 pub mod vocab;
 
 pub use crate::blank_node::{BlankNode, BlankNodeIdParseError, BlankNodeRef};
-pub use crate::cast_error::{TryFromTermError, TripleConstructionError};
+pub use crate::cast_error::{TripleConstructionError, TryFromTermError};
 pub use crate::dataset::Dataset;
 pub use crate::graph::Graph;
 pub use crate::literal::{Literal, LiteralRef};
