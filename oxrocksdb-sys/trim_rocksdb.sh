f239a24340d973cecef234439bb3b7c86058fc02
Tpt <thomaspt@hotmail.fr>
2024-05-18 15:22:38 +0200

    Upgrades dependencies

--- oxrocksdb-sys/trim_rocksdb.sh
+++ oxrocksdb-sys/trim_rocksdb.sh
@@ -2,3 +2,4 @@
 
 cd "$(dirname "$0")"/rocksdb || return
 rm -rf java .circleci .github build_tools coverage db_stress_tool docs examples fuzz java microbench ./**/*_test.cc
+git commit -a -m "Makes directory smaller by removing files not used by Oxigraph"
