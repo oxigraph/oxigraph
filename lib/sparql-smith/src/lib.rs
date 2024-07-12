32b04581f60dac2c401505dd0c58bb8fe2245e3f
Tpt <thomaspt@hotmail.fr>
2024-07-12 21:41:49 +0200

    Fuzzer: consider more literals

--- lib/sparql-smith/src/lib.rs
+++ lib/sparql-smith/src/lib.rs
@@ -4,29 +4,86 @@ use std::iter::once;
 use std::ops::ControlFlow;
 
 pub const DATA_TRIG: &str = "
-@prefix : <http://example.com/> .
+@prefix : <http://example.org/> .
 
 :1 :2 :3 , :4 ;
    :5 true , 1 , 1.0 , 1e0 .
 
 :3 :2 :4 ;
    :5 false , 0 , 0.0 , 0e0 .
+
+:1 {
+    :1 :2 :3 , 'foo' .
+}
+
+:2 {
+    :3 :2 :1 , false , 'bar'@en .
+}
+
+:3 {
+    :3 :2 :1 , true .
+}
 ";
 
 const NUMBER_OF_NAMED_NODES: u8 = 5;
 const NUMBER_OF_VARIABLES: u8 = 4;
-const LITERALS: [&str; 11] = [
+const LITERALS: [&str; 56] = [
     "\"foo\"",
     "\"foo\"^^<http://www.w3.org/2001/XMLSchema#string>",
     "\"foo\"@en",
+    "\"foo\"@en-us",
     "true",
     "false",
     "0",
     "0.0",
     "0e0",
+    "-0e0",
     "1",
     "1.0",
     "1e0",
+    "-1",
+    "-1.0",
+    "-1e0",
+    "\"INF\"^^<http://www.w3.org/2001/XMLSchema#double>",
+    "\"-INF\"^^<http://www.w3.org/2001/XMLSchema#double>",
+    "\"NaN\"^^<http://www.w3.org/2001/XMLSchema#double>",
+    "\"INF\"^^<http://www.w3.org/2001/XMLSchema#float>",
+    "\"-INF\"^^<http://www.w3.org/2001/XMLSchema#float>",
+    "\"NaN\"^^<http://www.w3.org/2001/XMLSchema#float>",
+    "\"0\"^^<http://www.w3.org/2001/XMLSchema#float>",
+    "\"-0\"^^<http://www.w3.org/2001/XMLSchema#float>",
+    "\"1\"^^<http://www.w3.org/2001/XMLSchema#float>",
+    "\"-1\"^^<http://www.w3.org/2001/XMLSchema#float>",
+    "\"P1YT1S\"^^<http://www.w3.org/2001/XMLSchema#duration>",
+    "\"P1Y\"^^<http://www.w3.org/2001/XMLSchema#duration>",
+    "\"PT1S\"^^<http://www.w3.org/2001/XMLSchema#duration>",
+    "\"P1Y\"^^<http://www.w3.org/2001/XMLSchema#yearMonthDuration>",
+    "\"PT1S\"^^<http://www.w3.org/2001/XMLSchema#dayTimeDuration>",
+    "\"2020-01-01T00:00:00\"^^<http://www.w3.org/2001/XMLSchema#dateTime>",
+    "\"2020-01-01T00:00:00Z\"^^<http://www.w3.org/2001/XMLSchema#dateTime>",
+    "\"2020-01-01T24:00:00Z\"^^<http://www.w3.org/2001/XMLSchema#dateTime>",
+    "\"2020-01-01T01:00:00.1-14:00\"^^<http://www.w3.org/2001/XMLSchema#dateTime>",
+    "\"2020-01-01T02:00:00.2+14:00\"^^<http://www.w3.org/2001/XMLSchema#dateTime>",
+    "\"2020-01-01T03:00:00.3-10:30\"^^<http://www.w3.org/2001/XMLSchema#dateTime>",
+    "\"2020-01-01T04:00:00.4+10:30\"^^<http://www.w3.org/2001/XMLSchema#dateTime>",
+    "\"2020-01-01\"^^<http://www.w3.org/2001/XMLSchema#date>",
+    "\"2020-01-01Z\"^^<http://www.w3.org/2001/XMLSchema#date>",
+    "\"2020-01-01-14:00\"^^<http://www.w3.org/2001/XMLSchema#date>",
+    "\"2020-01-01+14:00\"^^<http://www.w3.org/2001/XMLSchema#date>",
+    "\"2020-01-01-10:30\"^^<http://www.w3.org/2001/XMLSchema#date>",
+    "\"2020-01-01+10:30\"^^<http://www.w3.org/2001/XMLSchema#date>",
+    "\"00:00:00\"^^<http://www.w3.org/2001/XMLSchema#time>",
+    "\"00:00:00Z\"^^<http://www.w3.org/2001/XMLSchema#time>",
+    "\"24:00:00Z\"^^<http://www.w3.org/2001/XMLSchema#time>",
+    "\"01:00:00.1-14:00\"^^<http://www.w3.org/2001/XMLSchema#time>",
+    "\"02:00:00.2+14:00\"^^<http://www.w3.org/2001/XMLSchema#time>",
+    "\"03:00:00.3-10:30\"^^<http://www.w3.org/2001/XMLSchema#time>",
+    "\"04:00:00.4+10:30\"^^<http://www.w3.org/2001/XMLSchema#time>",
+    "\"2020\"^^<http://www.w3.org/2001/XMLSchema#gYear>",
+    "\"2020-01\"^^<http://www.w3.org/2001/XMLSchema#gYearMonth>",
+    "\"--01\"^^<http://www.w3.org/2001/XMLSchema#gMonth>",
+    "\"--01-01\"^^<http://www.w3.org/2001/XMLSchema#gMonthDay>",
+    "\"---01\"^^<http://www.w3.org/2001/XMLSchema#gDay>",
 ];
 
 pub struct Query {
