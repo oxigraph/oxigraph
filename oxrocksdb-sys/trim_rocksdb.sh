05612c6275c3a56b352c33eaeef3fbaee46b68cd
Tpt <thomaspt@hotmail.fr>
2024-12-18 17:56:07 +0100

    Readthedocs: fixes configuration

--- oxrocksdb-sys/trim_rocksdb.sh
+++ oxrocksdb-sys/trim_rocksdb.sh
@@ -2,4 +2,3 @@
 
 cd "$(dirname "$0")"/rocksdb || return
 rm -rf java .circleci .github build_tools coverage db_stress_tool docs examples fuzz java microbench ./**/*_test.cc
-git commit -a -m "Makes directory smaller by removing files not used by Oxigraph"
