4aaa9428098a04d2088b502bccf1517dbf970629
Tpt <thomaspt@hotmail.fr>
2025-03-25 21:27:41 +0100

    Python: use UV for dependencies management

--- .github/workflows/manylinux_build.sh
+++ .github/workflows/manylinux_build.sh
@@ -7,17 +7,15 @@ chmod +x rustup-init
 ./rustup-init -y --profile minimal
 source "$HOME/.cargo/env"
 cd python
-uv venv
-uv pip install -r requirements.build.txt
-source .venv/bin/activate
-maturin develop --release
-python generate_stubs.py pyoxigraph pyoxigraph.pyi --ruff
-maturin build --release --features abi3 --compatibility manylinux2014
+uv sync --locked --only-group build
+uv run maturin develop --release --features abi3
+uv run python generate_stubs.py pyoxigraph pyoxigraph.pyi --ruff
+uv run maturin build --release --features abi3 --compatibility manylinux2014
 if [ %for_each_version% ]; then
   for VERSION in 8 9 10 11 12 13 13t; do
-    maturin build --release --interpreter "python3.$VERSION" --compatibility manylinux2014
+    uv run maturin build --release --interpreter "python3.$VERSION" --compatibility manylinux2014
   done
-  for VERSION in 9 10; do
-    maturin build --release --interpreter "pypy3.$VERSION" --compatibility manylinux2014
+  for VERSION in 9 10 11; do
+    uv run maturin build --release --interpreter "pypy3.$VERSION" --compatibility manylinux2014
   done
 fi
