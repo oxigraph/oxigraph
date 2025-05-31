0ccf51dab7b16cc700d0cb18550f207c21cf4a4c
Thomas Tanon <thomas@pellissier-tanon.fr>
2025-05-31 17:09:22 +0200

    Improve CLI documentation

--- cli/src/cli.rs
+++ cli/src/cli.rs
@@ -24,7 +24,7 @@ pub enum Command {
         /// Allows cross-origin requests
         #[arg(long)]
         cors: bool,
-        /// If the SPARQL queries should look for triples in all the dataset graphs by default (ie. without `GRAPH` operations)
+        /// If the SPARQL queries should look for triples in all the dataset graphs by default (i.e., without `GRAPH` operations)
         ///
         /// This is equivalent as setting the union-default-graph option in all SPARQL queries
         #[arg(long)]
@@ -32,8 +32,8 @@ pub enum Command {
     },
     /// Start Oxigraph HTTP server in read-only mode
     ///
-    /// It allows to read the database while other processes are also reading it.
-    /// Opening as read-only while having an other process writing the database is undefined behavior.
+    /// It allows reading the database while other processes are also reading it.
+    /// Opening as read-only while having another process writing the database is undefined behavior.
     ServeReadOnly {
         /// Directory in which Oxigraph data are persisted
         #[arg(short, long, value_hint = ValueHint::DirPath)]
@@ -44,7 +44,7 @@ pub enum Command {
         /// Allow cross-origin requests
         #[arg(long)]
         cors: bool,
-        /// If the SPARQL queries should look for triples in all the dataset graphs by default (ie. without `GRAPH` operations)
+        /// If the SPARQL queries should look for triples in all the dataset graphs by default (i.e. without `GRAPH` operations)
         ///
         /// This is equivalent as setting the union-default-graph option in all SPARQL queries
         #[arg(long)]
@@ -53,10 +53,10 @@ pub enum Command {
     /// Create a database backup into a target directory
     ///
     /// After its creation, the backup is usable a separated Oxigraph database
-    /// and operates independently from the original database.
+    /// and operates independently of the original database.
     ///
     /// If the target directory is in the same file system as the current database,
-    /// the database content will not be fully copied
+    /// the database content will not be fully copied,
     /// but hard links will be used to point to the original database immutable snapshots.
     /// This allows cheap regular backups.
     ///
@@ -78,16 +78,17 @@ pub enum Command {
         location: PathBuf,
         /// File(s) to load
         ///
-        /// If multiple files are provided they are loaded in parallel.
+        /// If multiple files are provided, they are loaded in parallel.
         ///
-        /// If no file is given, stdin is read.
+        /// If no file is given, stdin is used as if it were the input file content.
+        /// In this case, the content format must be specified using the --format option.
         #[arg(short, long, num_args = 0.., value_hint = ValueHint::FilePath)]
         file: Vec<PathBuf>,
         /// The format of the file(s) to load
         ///
         /// It can be an extension like "nt" or a MIME type like "application/n-triples".
         ///
-        /// By default the format is guessed from the loaded file extension.
+        /// By default, the format is guessed from the loaded file extension.
         #[arg(long, required_unless_present = "file")]
         format: Option<String>,
         /// Base IRI of the file(s) to load
@@ -95,12 +96,12 @@ pub enum Command {
         base: Option<String>,
         /// Attempt to keep loading even if the data file is invalid
         ///
-        /// This disables most of validation on RDF content.
+        /// This disables most of the validation on RDF content.
         #[arg(long)]
         lenient: bool,
         /// Name of the graph to load the data to
         ///
-        /// By default the default graph is used.
+        /// By default, the default graph is used.
         ///
         /// Only available when loading a graph file (N-Triples, Turtle...) and not a dataset file (N-Quads, TriG...).
         #[arg(long, value_hint = ValueHint::Url)]
@@ -114,21 +115,22 @@ pub enum Command {
         /// File to dump to
         ///
         /// If no file is given, stdout is used.
+        /// In this case, the output format must be specified using the --format option.
         #[arg(short, long, value_hint = ValueHint::FilePath)]
         file: Option<PathBuf>,
         /// The format of the file(s) to dump
         ///
         /// It can be an extension like "nt" or a MIME type like "application/n-triples".
         ///
-        /// By default the format is guessed from the target file extension.
+        /// By default, the format is guessed from the target file extension.
         #[arg(long, required_unless_present = "file")]
         format: Option<String>,
         /// Name of the graph to dump
         ///
         /// Use "default" to dump the default graph.
         ///
-        /// By default all graphs are dumped if the output format supports datasets.
-        /// If the format does not support named graph, then this parameter must be set.
+        /// By default, all graphs are dumped if the output format supports datasets.
+        /// If the format does not support named graphs, then this parameter must be set.
         #[arg(long, value_hint = ValueHint::Url)]
         graph: Option<String>,
     },
@@ -153,25 +155,26 @@ pub enum Command {
         /// File in which the query results will be stored
         ///
         /// If no file is given, stdout is used.
+        /// In this case, the output format must be specified using the --format option.
         #[arg(short, long, value_hint = ValueHint::FilePath)]
         results_file: Option<PathBuf>,
         /// The results format
         ///
         /// It can be an extension like "nt" or a MIME type like "application/n-triples".
         ///
-        /// By default the format is guessed from the results file extension.
+        /// By default, the format is guessed from the results file extension.
         #[arg(long, required_unless_present = "results_file")]
         results_format: Option<String>,
         /// Print to stderr a human-readable explanation of the query evaluation
         ///
-        /// Use the stats option to print also query evaluation statistics.
+        /// Use the --stats option to print also query evaluation statistics.
         #[arg(long, conflicts_with = "explain_file")]
         explain: bool,
         /// Write to the given file an explanation of the query evaluation
         ///
-        /// If the file extension is .json the JSON format is used, if .txt a human readable format is used.
+        /// If the file extension is ".json" the JSON format is used, if ".txt" a human-readable format is used.
         ///
-        /// Use the stats option to print also query evaluation statistics.
+        /// Use the --stats option to print also query evaluation statistics.
         #[arg(long, conflicts_with = "explain", value_hint = ValueHint::FilePath)]
         explain_file: Option<PathBuf>,
         /// Compute some evaluation statistics to print as part of the query explanations
@@ -205,24 +208,25 @@ pub enum Command {
     /// Optimize the database storage
     ///
     /// Done by default in the background when serving requests.
-    /// It is likely to not be useful in most of cases except if you provide a read-only SPARQL endpoint under heavy load.
+    /// It is likely to not be useful in most of the cases except if you provide a read-only SPARQL endpoint under heavy load.
     Optimize {
         /// Directory in which Oxigraph data are persisted
         #[arg(short, long, value_hint = ValueHint::DirPath)]
         location: PathBuf,
     },
-    /// Convert a RDF serialization from one format to an other
+    /// Convert an RDF file from one format to another
     Convert {
         /// File to convert from
         ///
-        /// If no file is given, stdin is read.
+        /// If no file is given, stdin is used as if it were the input file content.
+        /// In this case, the content format must be specified using the --format option.
         #[arg(short, long, value_hint = ValueHint::FilePath)]
         from_file: Option<PathBuf>,
         /// The format of the file(s) to convert from
         ///
         /// It can be an extension like "nt" or a MIME type like "application/n-triples".
         ///
-        /// By default the format is guessed from the input file extension.
+        /// By default, the format is guessed from the input file extension.
         #[arg(long, required_unless_present = "from_file")]
         from_format: Option<String>,
         /// Base IRI of the file to read
@@ -230,14 +234,15 @@ pub enum Command {
         from_base: Option<String>,
         /// File to convert to
         ///
-        /// If no file is given, stdout is written.
+        /// If no file is given, stdout is used.
+        /// In this case, the output format must be specified using the --to-format option.
         #[arg(short, long, value_hint = ValueHint::FilePath)]
         to_file: Option<PathBuf>,
         /// The format of the file(s) to convert to
         ///
         /// It can be an extension like "nt" or a MIME type like "application/n-triples".
         ///
-        /// By default the format is guessed from the target file extension.
+        /// By default, the format is guessed from the target file extension.
         #[arg(long, required_unless_present = "to_file")]
         to_format: Option<String>,
         /// Base IRI of the file to write
@@ -245,12 +250,12 @@ pub enum Command {
         to_base: Option<String>,
         /// Attempt to keep converting even if the data file is invalid
         ///
-        /// This disables most of validation on RDF content.
+        /// This disables most of the validation on RDF content.
         #[arg(long)]
         lenient: bool,
         /// Only load the given named graph from the input file
         ///
-        /// By default all graphs are loaded.
+        /// By default, all graphs are loaded.
         #[arg(long, conflicts_with = "from_default_graph", value_hint = ValueHint::Url)]
         from_graph: Option<String>,
         /// Only load the default graph from the input file
@@ -258,7 +263,7 @@ pub enum Command {
         from_default_graph: bool,
         /// Name of the graph to map the default graph to
         ///
-        /// By default the default graph is used.
+        /// By default, the default graph is used.
         #[arg(long, value_hint = ValueHint::Url)]
         to_graph: Option<String>,
     },
