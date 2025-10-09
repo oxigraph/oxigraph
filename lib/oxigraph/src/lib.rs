941cc89b0ac1e574fff78deff49bd70e3316135f
Thomas Tanon <thomas@pellissier-tanon.fr>
2025-10-09 21:44:00 +0200

    doc_auto_cfg -> doc_cfg

--- lib/oxigraph/src/lib.rs
+++ lib/oxigraph/src/lib.rs
@@ -1,7 +1,7 @@
 #![doc = include_str!("../README.md")]
 #![doc(test(attr(deny(warnings))))]
 #![doc(test(attr(allow(deprecated))))]
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 #![doc(html_favicon_url = "https://raw.githubusercontent.com/oxigraph/oxigraph/main/logo.svg")]
 #![doc(html_logo_url = "https://raw.githubusercontent.com/oxigraph/oxigraph/main/logo.svg")]
 
