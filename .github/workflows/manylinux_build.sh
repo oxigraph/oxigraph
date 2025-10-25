c60e01f048be1b6f51d3f442bf92591cc39bdb29
Thomas Tanon <thomas@pellissier-tanon.fr>
2025-10-25 11:59:59 +0200

    Bump to manylinux_2_28
    Support C++20 compilation

--- .github/workflows/manylinux_build.sh
+++ .github/workflows/manylinux_build.sh
@@ -1,21 +1,19 @@
 cd /workdir
-yum -y install centos-release-scl-rh
-yum -y install llvm-toolset-7.0
-source scl_source enable llvm-toolset-7.0
+dnf install -y clang
 curl https://static.rust-lang.org/rustup/dist/%arch%-unknown-linux-gnu/rustup-init --output rustup-init
 chmod +x rustup-init
 ./rustup-init -y --profile minimal
 source "$HOME/.cargo/env"
 cd python
-uv run maturin develop --release --features abi3
-uv run python generate_stubs.py pyoxigraph pyoxigraph.pyi --ruff
+uv run --locked --only-dev maturin develop --release --features abi3
+uv run --locked --only-dev python generate_stubs.py pyoxigraph pyoxigraph.pyi --ruff
 rm -rf ../target/wheels
-uv run maturin build --release --features abi3 --compatibility manylinux2014
+uv run --locked --only-dev maturin build --release --features abi3 --compatibility manylinux_2_28
 if [ %for_each_version% ]; then
   for VERSION in 8 9 10 11 12 13 13t 14 14t; do
-    uv run maturin build --release --interpreter "python3.$VERSION" --compatibility manylinux2014
+    uv run --locked --only-dev maturin build --release --interpreter "python3.$VERSION" --compatibility manylinux_2_28
   done
-  uv run maturin build --release --interpreter "pypy3.11" --compatibility manylinux2014
+  uv run --locked --only-dev maturin build --release --interpreter "pypy3.11" --compatibility manylinux_2_28
 fi
 cd ../cli
-uvx maturin build --release --no-default-features --features rustls-native,geosparql,rdf-12 --compatibility manylinux2014
+uvx maturin build --release --no-default-features --features rustls-native,geosparql,rdf-12 --compatibility manylinux_2_28
