afc85d906ad2452c63d8a0d2fde32a46d62b5c68
Thomas Tanon <thomas@pellissier-tanon.fr>
2026-01-25 22:12:29 +0100

    Stop building CLI musl builds
    Needs complex fixes in RocksDB toolchain

--- .github/workflows/musllinux_build.sh
+++ .github/workflows/musllinux_build.sh
@@ -9,5 +9,3 @@ uv run --locked --only-dev maturin develop --release --features abi3
 uv run --locked --only-dev python generate_stubs.py pyoxigraph pyoxigraph.pyi --ruff
 rm -rf ../target/wheels
 uv run --locked --only-dev maturin build --release --features abi3 --compatibility musllinux_1_2
-cd ../cli
-uvx maturin build --release --no-default-features --features rustls-native,geosparql,rdf-12 --compatibility musllinux_1_2
