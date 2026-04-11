ab473775a8ce2e77e4a3813c5bdf7e385d6fa0d6
Thomas Tanon <thomas@pellissier-tanon.fr>
2026-04-11 11:25:00 +0200

    Bump the RDF/SPARQL testsuite

--- testsuite/tests/sparql.rs
+++ testsuite/tests/sparql.rs
@@ -13,7 +13,6 @@ fn sparql10_w3c_query_syntax_testsuite() -> Result<()> {
     )
 }
 
-#[cfg(not(windows))] // Tests don't like git auto "\r\n" on Windows
 #[test]
 fn sparql10_w3c_query_evaluation_testsuite() -> Result<()> {
     check_testsuite(
@@ -58,6 +57,9 @@ fn sparql11_query_w3c_evaluation_testsuite() -> Result<()> {
             "http://www.w3.org/2009/sparql/docs/tests/data-sparql11/property-path/manifest#zero_or_more_set_end",
             "http://www.w3.org/2009/sparql/docs/tests/data-sparql11/property-path/manifest#zero_or_one_set_start",
             "http://www.w3.org/2009/sparql/docs/tests/data-sparql11/property-path/manifest#zero_or_one_set_end",
+            // Our GROUP_CONCAT is wrong
+            "http://www.w3.org/2009/sparql/docs/tests/data-sparql11/aggregates/manifest#agg-groupconcat-04",
+            "http://www.w3.org/2009/sparql/docs/tests/data-sparql11/aggregates/manifest#agg-groupconcat-06",
         ],
     )
 }
