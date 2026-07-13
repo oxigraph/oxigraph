31a3cfc99d3c46910bd40dd0281e65c82891e904
Thomas Tanon <thomas@pellissier-tanon.fr>
2026-07-13 15:14:56 +0200

    SPARQL Update: small code simplification

--- lib/spareval/src/update.rs
+++ lib/spareval/src/update.rs
@@ -1,7 +1,7 @@
 use crate::{QueryEvaluationError, QuerySolutionIter};
 #[cfg(feature = "sparql-12")]
 use oxrdf::Triple;
-use oxrdf::{BlankNode, GraphName, NamedNode, Quad, Term, Variable};
+use oxrdf::{BlankNode, GraphName, NamedNode, Quad, Term};
 use rustc_hash::FxHashMap;
 use sparesults::QuerySolution;
 use spargebra::term::{
@@ -38,26 +38,37 @@ impl<'a, 'b> DeleteInsertIter<'a, 'b> {
         insert: &'b [QuadPattern],
         without_optimizations: bool,
     ) -> Self {
-        let empty_solution =
-            QuerySolution::from((Vec::<Variable>::new(), Vec::<Option<Term>>::new()));
         let mut ground_delete = Vec::new();
         let mut variable_delete = Vec::new();
-        for quad in delete {
-            if without_optimizations || ground_quad_pattern_has_variable(quad) {
-                variable_delete.push(quad);
+        for quad_pattern in delete {
+            let empty_solution = QuerySolution::default();
+            if without_optimizations {
+                variable_delete.push(quad_pattern);
+            } else if let Some(quad) = fill_ground_quad_pattern(quad_pattern, &empty_solution) {
+                ground_delete.push(quad);
             } else {
-                ground_delete.extend(fill_ground_quad_pattern(quad, &empty_solution));
+                variable_delete.push(quad_pattern);
             }
         }
 
         let mut ground_insert = Vec::new();
         let mut variable_insert = Vec::new();
-        let mut bnodes = FxHashMap::default();
-        for quad in insert {
-            if without_optimizations || quad_pattern_needs_solution(quad) {
-                variable_insert.push(quad);
+        for quad_pattern in insert {
+            let empty_solution = QuerySolution::default();
+            let mut blank_nodes = FxHashMap::default();
+            if without_optimizations {
+                variable_insert.push(quad_pattern);
+            } else if let Some(quad) =
+                fill_quad_pattern(quad_pattern, &empty_solution, &mut blank_nodes)
+            {
+                if blank_nodes.is_empty() {
+                    // We do this check to ensure we don't have to emit a new blank node for each solution.
+                    ground_insert.push(quad);
+                } else {
+                    variable_insert.push(quad_pattern);
+                }
             } else {
-                ground_insert.extend(fill_quad_pattern(quad, &empty_solution, &mut bnodes));
+                variable_insert.push(quad_pattern);
             }
         }
 
@@ -105,60 +116,6 @@ impl Iterator for DeleteInsertIter<'_, '_> {
             self.bnodes.clear();
         }
     }
-}
-
-fn ground_quad_pattern_has_variable(quad: &GroundQuadPattern) -> bool {
-    ground_term_pattern_has_variable(&quad.subject)
-        || named_node_pattern_has_variable(&quad.predicate)
-        || ground_term_pattern_has_variable(&quad.object)
-        || graph_name_pattern_has_variable(&quad.graph_name)
-}
-
-fn ground_term_pattern_has_variable(term: &GroundTermPattern) -> bool {
-    match term {
-        GroundTermPattern::NamedNode(_) | GroundTermPattern::Literal(_) => false,
-        #[cfg(feature = "sparql-12")]
-        GroundTermPattern::Triple(triple) => ground_triple_pattern_has_variable(triple),
-        GroundTermPattern::Variable(_) => true,
-    }
-}
-
-#[cfg(feature = "sparql-12")]
-fn ground_triple_pattern_has_variable(triple: &GroundTriplePattern) -> bool {
-    ground_term_pattern_has_variable(&triple.subject)
-        || named_node_pattern_has_variable(&triple.predicate)
-        || ground_term_pattern_has_variable(&triple.object)
-}
-
-fn quad_pattern_needs_solution(quad: &QuadPattern) -> bool {
-    insert_term_pattern_needs_solution(&quad.subject)
-        || named_node_pattern_has_variable(&quad.predicate)
-        || insert_term_pattern_needs_solution(&quad.object)
-        || graph_name_pattern_has_variable(&quad.graph_name)
-}
-
-fn insert_term_pattern_needs_solution(term: &TermPattern) -> bool {
-    match term {
-        TermPattern::NamedNode(_) | TermPattern::Literal(_) => false,
-        TermPattern::BlankNode(_) | TermPattern::Variable(_) => true,
-        #[cfg(feature = "sparql-12")]
-        TermPattern::Triple(triple) => triple_pattern_needs_solution(triple),
-    }
-}
-
-#[cfg(feature = "sparql-12")]
-fn triple_pattern_needs_solution(triple: &TriplePattern) -> bool {
-    insert_term_pattern_needs_solution(&triple.subject)
-        || named_node_pattern_has_variable(&triple.predicate)
-        || insert_term_pattern_needs_solution(&triple.object)
-}
-
-fn named_node_pattern_has_variable(term: &NamedNodePattern) -> bool {
-    matches!(term, NamedNodePattern::Variable(_))
-}
-
-fn graph_name_pattern_has_variable(term: &GraphNamePattern) -> bool {
-    matches!(term, GraphNamePattern::Variable(_))
 }
 
 fn fill_quad_pattern(
@@ -285,227 +242,4 @@ fn fill_ground_triple_pattern(
 
 fn convert_blank_node(node: &BlankNode, bnodes: &mut FxHashMap<BlankNode, BlankNode>) -> BlankNode {
     bnodes.entry(node.clone()).or_default().clone()
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use oxrdf::{Literal, Variable};
-    use std::sync::Arc;
-
-    fn ex(name: &str) -> NamedNode {
-        NamedNode::new_unchecked(format!("http://example.com/{name}"))
-    }
-
-    fn delete_pattern(
-        subject: impl Into<GroundTermPattern>,
-        predicate: NamedNode,
-        object: impl Into<GroundTermPattern>,
-    ) -> GroundQuadPattern {
-        GroundQuadPattern {
-            subject: subject.into(),
-            predicate: predicate.into(),
-            object: object.into(),
-            graph_name: GraphNamePattern::DefaultGraph,
-        }
-    }
-
-    fn insert_pattern(
-        subject: impl Into<TermPattern>,
-        predicate: NamedNode,
-        object: impl Into<TermPattern>,
-    ) -> QuadPattern {
-        QuadPattern {
-            subject: subject.into(),
-            predicate: predicate.into(),
-            object: object.into(),
-            graph_name: GraphNamePattern::DefaultGraph,
-        }
-    }
-
-    fn solution_iter(
-        variables: Vec<Variable>,
-        rows: Vec<Vec<Option<Term>>>,
-    ) -> QuerySolutionIter<'static> {
-        let variables: Arc<[Variable]> = variables.into();
-        QuerySolutionIter::from_tuples(
-            variables,
-            rows.into_iter().map(Ok::<_, QueryEvaluationError>),
-        )
-    }
-
-    fn collect(
-        delete: &[GroundQuadPattern],
-        insert: &[QuadPattern],
-        variables: Vec<Variable>,
-        rows: Vec<Vec<Option<Term>>>,
-    ) -> Result<Vec<DeleteInsertQuad>, QueryEvaluationError> {
-        DeleteInsertIter::new(solution_iter(variables, rows), delete, insert, false).collect()
-    }
-
-    fn deleted_quads(results: &[DeleteInsertQuad]) -> Vec<Quad> {
-        results
-            .iter()
-            .filter_map(|result| match result {
-                DeleteInsertQuad::Delete(quad) => Some(quad.clone()),
-                DeleteInsertQuad::Insert(_) => None,
-            })
-            .collect()
-    }
-
-    fn inserted_quads(results: &[DeleteInsertQuad]) -> Vec<Quad> {
-        results
-            .iter()
-            .filter_map(|result| match result {
-                DeleteInsertQuad::Delete(_) => None,
-                DeleteInsertQuad::Insert(quad) => Some(quad.clone()),
-            })
-            .collect()
-    }
-
-    #[test]
-    fn ground_insert_is_emitted_once_with_multiple_solutions() -> Result<(), QueryEvaluationError> {
-        let s = Variable::new_unchecked("s");
-        let p = ex("p");
-        let old = ex("old");
-        let inserted = Quad::new(
-            ex("ground"),
-            p.clone(),
-            ex("inserted"),
-            GraphName::DefaultGraph,
-        );
-        let delete = [delete_pattern(s.clone(), p.clone(), old.clone())];
-        let insert = [insert_pattern(ex("ground"), p, ex("inserted"))];
-
-        let results = collect(
-            &delete,
-            &insert,
-            vec![s],
-            vec![vec![Some(ex("s1").into())], vec![Some(ex("s2").into())]],
-        )?;
-
-        assert_eq!(deleted_quads(&results).len(), 2);
-        assert_eq!(inserted_quads(&results), vec![inserted]);
-        Ok(())
-    }
-
-    #[test]
-    fn ground_insert_is_not_emitted_without_solutions() -> Result<(), QueryEvaluationError> {
-        let p = ex("p");
-        let delete = [];
-        let insert = [insert_pattern(ex("ground"), p, ex("inserted"))];
-
-        let results = collect(&delete, &insert, Vec::new(), Vec::new())?;
-
-        assert!(results.is_empty());
-        Ok(())
-    }
-
-    #[test]
-    fn insert_with_blank_node_is_emitted_for_each_solution() -> Result<(), QueryEvaluationError> {
-        let p = ex("p");
-        let delete = [];
-        let insert = [insert_pattern(
-            BlankNode::new_unchecked("b"),
-            p,
-            Literal::from("value"),
-        )];
-
-        let results = collect(&delete, &insert, Vec::new(), vec![Vec::new(), Vec::new()])?;
-        let inserted = inserted_quads(&results);
-
-        assert_eq!(inserted.len(), 2);
-        assert_ne!(inserted[0].subject, inserted[1].subject);
-        Ok(())
-    }
-
-    #[test]
-    fn mixed_insert_hoists_only_ground_templates() -> Result<(), QueryEvaluationError> {
-        let o = Variable::new_unchecked("o");
-        let p = ex("p");
-        let ground_insert = Quad::new(
-            ex("ground"),
-            p.clone(),
-            ex("ground-o"),
-            GraphName::DefaultGraph,
-        );
-        let first_variable_insert =
-            Quad::new(ex("variable"), p.clone(), ex("o1"), GraphName::DefaultGraph);
-        let second_variable_insert =
-            Quad::new(ex("variable"), p.clone(), ex("o2"), GraphName::DefaultGraph);
-        let delete = [];
-        let insert = [
-            insert_pattern(ex("ground"), p.clone(), ex("ground-o")),
-            insert_pattern(ex("variable"), p, o.clone()),
-        ];
-
-        let results = collect(
-            &delete,
-            &insert,
-            vec![o],
-            vec![vec![Some(ex("o1").into())], vec![Some(ex("o2").into())]],
-        )?;
-        let inserted = inserted_quads(&results);
-
-        assert_eq!(inserted.len(), 3);
-        assert_eq!(
-            inserted
-                .iter()
-                .filter(|quad| **quad == ground_insert)
-                .count(),
-            1
-        );
-        assert!(inserted.contains(&first_variable_insert));
-        assert!(inserted.contains(&second_variable_insert));
-        Ok(())
-    }
-
-    #[test]
-    fn ground_delete_is_emitted_once_with_multiple_solutions() -> Result<(), QueryEvaluationError> {
-        let p = ex("p");
-        let deleted = Quad::new(
-            ex("ground"),
-            p.clone(),
-            ex("deleted"),
-            GraphName::DefaultGraph,
-        );
-        let delete = [delete_pattern(ex("ground"), p, ex("deleted"))];
-        let insert = [];
-
-        let results = collect(&delete, &insert, Vec::new(), vec![Vec::new(), Vec::new()])?;
-
-        assert_eq!(deleted_quads(&results), vec![deleted]);
-        Ok(())
-    }
-
-    #[cfg(feature = "sparql-12")]
-    #[test]
-    fn nested_variable_in_quoted_triple_insert_is_not_hoisted() -> Result<(), QueryEvaluationError>
-    {
-        let o = Variable::new_unchecked("o");
-        let p = ex("p");
-        let quoted = TriplePattern {
-            subject: ex("quoted-s").into(),
-            predicate: p.clone().into(),
-            object: o.clone().into(),
-        };
-        let delete = [];
-        let insert = [insert_pattern(
-            ex("subject"),
-            p,
-            TermPattern::Triple(Box::new(quoted)),
-        )];
-
-        let results = collect(
-            &delete,
-            &insert,
-            vec![o],
-            vec![vec![Some(ex("o1").into())], vec![Some(ex("o2").into())]],
-        )?;
-        let inserted = inserted_quads(&results);
-
-        assert_eq!(inserted.len(), 2);
-        assert_ne!(inserted[0].object, inserted[1].object);
-        Ok(())
-    }
 }
